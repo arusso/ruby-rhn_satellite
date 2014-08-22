fc534d2e3c48e1495c2f8d4d72196f1be11251d2
Aaron Russo <arusso@berkeley.edu>
2014-08-21 22:06:37 -0700

    add channel.listMyChannels

--- lib/rhn_satellite/channel.rb
+++ lib/rhn_satellite/channel.rb
@@ -1,5 +1,13 @@
 module RhnSatellite
   class Channel < RhnSatellite::Connection::Base
+    API_NS="channel"
+
     collection 'channel.listAllChannels'
+
+    class << self
+      def list_my_channels
+        base.default_call("#{API_NS}.listMyChannels")
+      end
+    end
   end
 end
